c912c9d9a91e276794b4b678389fcf6861a49fd4
Vinicius <medrado.jobs@gmail.com>
2026-03-13 02:30:50 -0300

    Atualizando callback e requirements para deploy Render

--- callback.py
+++ callback.py
@@ -15,4 +15,6 @@ def ml_callback():
     return "✅ Cliente autorizado e tokens atualizados!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
